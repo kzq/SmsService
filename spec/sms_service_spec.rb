001edd1a40f59cd72d1b37371f783ddf9273db95
kzq <kami_ravian@yahoo.com>
2016-04-16 13:25:10 +0100

    Added spec files for rspec

---
+++ spec/sms_service_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe SmsService do 
+  it "do it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+Bundler.setup
+require 'lib/sms_service'
+RSpec.configure do |config|
+  # some (optional) config here
+end
